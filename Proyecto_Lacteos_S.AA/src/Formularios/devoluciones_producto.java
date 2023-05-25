58ed2b9f26aceb3c2a771735ea989c7e7c00323f
Diego03 <dgarciar30@miumg.edu.gt>
2023-05-25 14:09:28 -0600

    Campo "motivo"
    Se agrego el campo "motivo" en el formulario para las devoluiones

--- Proyecto_Lacteos_S.AA/src/Formularios/devoluciones_producto.java
+++ Proyecto_Lacteos_S.AA/src/Formularios/devoluciones_producto.java
@@ -51,9 +51,10 @@ public class devoluciones_producto extends javax.swing.JFrame {
         model.addColumn("CANTIDAD");
         model.addColumn("FECHA");
         model.addColumn("CLIENTE");
+        model.addColumn("Motivo");
         tbldevoluciones.setModel(model);
         
-        String [] datos = new String[5];
+        String [] datos = new String[6];
         try{
          st = conexion.createStatement();
          ResultSet rs = st.executeQuery(sql);
@@ -64,6 +65,7 @@ public class devoluciones_producto extends javax.swing.JFrame {
             datos[2]=rs.getString(3);
             datos[3]=rs.getString(4);
             datos[4]=rs.getString(5);
+            datos[5]=rs.getString(6);
             model.addRow(datos);
          }
         }catch(SQLException e){
@@ -95,6 +97,8 @@ public class devoluciones_producto extends javax.swing.JFrame {
         jtextdate = new javax.swing.JTextField();
         jScrollPane1 = new javax.swing.JScrollPane();
         tbldevoluciones = new javax.swing.JTable();
+        jLabel5 = new javax.swing.JLabel();
+        jtextmot = new javax.swing.JTextField();
 
         jToggleButton1.setText("jToggleButton1");
 
@@ -156,58 +160,72 @@ public class devoluciones_producto extends javax.swing.JFrame {
 
         tbldevoluciones.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
-                {null, null, null, null, null},
-                {null, null, null, null, null},
-                {null, null, null, null, null},
-                {null, null, null, null, null}
+                {null, null, null, null, null, null},
+                {null, null, null, null, null, null},
+                {null, null, null, null, null, null},
+                {null, null, null, null, null, null}
             },
             new String [] {
-                "ID DEVOLUCION", "PRODUCTO", "CANTIDAD", "FECHA", "CLIENTE"
+                "ID DEVOLUCION", "PRODUCTO", "CANTIDAD", "FECHA", "CLIENTE", "MOTIVO"
             }
         ));
         jScrollPane1.setViewportView(tbldevoluciones);
+
+        jLabel5.setFont(new java.awt.Font("Georgia", 0, 12)); // NOI18N
+        jLabel5.setForeground(new java.awt.Color(102, 0, 204));
+        jLabel5.setText("Motivo");
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel1Layout.createSequentialGroup()
-                .addGap(204, 204, 204)
+                .addGap(206, 206, 206)
                 .addComponent(btnguardar)
-                .addGap(57, 57, 57)
+                .addGap(49, 49, 49)
                 .addComponent(btnsalir)
                 .addGap(0, 0, Short.MAX_VALUE))
             .addGroup(jPanel1Layout.createSequentialGroup()
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addGap(34, 34, 34)
-                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                            .addGroup(jPanel1Layout.createSequentialGroup()
-                                .addComponent(jLabel1)
-                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                                .addComponent(cmbboxproducto, 0, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-                            .addGroup(jPanel1Layout.createSequentialGroup()
-                                .addComponent(jLabel2)
-                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                                .addComponent(jtextcan, javax.swing.GroupLayout.PREFERRED_SIZE, 100, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                        .addGap(60, 60, 60)
                         .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(jLabel3)
-                            .addComponent(jLabel4))
-                        .addGap(18, 18, 18)
-                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                            .addComponent(cmbboxcliente, 0, 130, Short.MAX_VALUE)
-                            .addComponent(jtextdate)))
-                    .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addGap(108, 108, 108)
-                        .addComponent(jLabel6))
-                    .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addGap(15, 15, 15)
-                        .addComponent(jLabel10))
-                    .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addContainerGap()
+                            .addGroup(jPanel1Layout.createSequentialGroup()
+                                .addGap(34, 34, 34)
+                                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                    .addGroup(jPanel1Layout.createSequentialGroup()
+                                        .addComponent(jLabel1)
+                                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                        .addComponent(cmbboxproducto, 0, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                        .addGap(60, 60, 60)
+                                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                            .addComponent(jLabel3)
+                                            .addComponent(jLabel4))
+                                        .addGap(18, 18, 18)
+                                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                                            .addComponent(cmbboxcliente, 0, 130, Short.MAX_VALUE)
+                                            .addComponent(jtextdate)))
+                                    .addGroup(jPanel1Layout.createSequentialGroup()
+                                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                                            .addComponent(jLabel5)
+                                            .addComponent(jLabel2))
+                                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                            .addGroup(jPanel1Layout.createSequentialGroup()
+                                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                                .addComponent(jtextcan, javax.swing.GroupLayout.PREFERRED_SIZE, 100, javax.swing.GroupLayout.PREFERRED_SIZE))
+                                            .addGroup(jPanel1Layout.createSequentialGroup()
+                                                .addGap(20, 20, 20)
+                                                .addComponent(jtextmot, javax.swing.GroupLayout.PREFERRED_SIZE, 366, javax.swing.GroupLayout.PREFERRED_SIZE))))))
+                            .addGroup(jPanel1Layout.createSequentialGroup()
+                                .addGap(108, 108, 108)
+                                .addComponent(jLabel6))
+                            .addGroup(jPanel1Layout.createSequentialGroup()
+                                .addGap(15, 15, 15)
+                                .addComponent(jLabel10)))
+                        .addGap(0, 61, Short.MAX_VALUE))
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                        .addGap(0, 6, Short.MAX_VALUE)
                         .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 539, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addContainerGap())
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -226,13 +244,17 @@ public class devoluciones_producto extends javax.swing.JFrame {
                     .addComponent(jLabel4)
                     .addComponent(jtextcan, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(cmbboxcliente, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addGap(35, 35, 35)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel5)
+                    .addComponent(jtextmot, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(26, 26, 26)
                 .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 149, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(18, 18, 18)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addComponent(btnguardar)
                     .addComponent(btnsalir))
-                .addGap(145, 145, 145)
+                .addGap(90, 90, 90)
                 .addComponent(jLabel6, javax.swing.GroupLayout.PREFERRED_SIZE, 22, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
@@ -245,9 +267,7 @@ public class devoluciones_producto extends javax.swing.JFrame {
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, 385, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(0, 0, Short.MAX_VALUE))
+            .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, 470, Short.MAX_VALUE)
         );
 
         pack();
@@ -268,7 +288,7 @@ public class devoluciones_producto extends javax.swing.JFrame {
         //en la tabla "stock"
         cls_conexion mysql= new cls_conexion();
         Connection cn=mysql.getConnection();
-        String pro, date, cl="";
+        String mot, pro, date, cl="";
         int iddev, can=0;
         
         String sSQL="";
@@ -279,8 +299,9 @@ public class devoluciones_producto extends javax.swing.JFrame {
         can= Integer.parseInt(jtextcan.getText());
         date= jtextdate.getText();
         cl = cmbboxcliente.getSelectedItem().toString();
-        sSQL="INSERT INTO devoluciones(iddevoluciones, producto, cantidad, fecha, cliente)"
-                +"VALUES(?,?,?,?,?)";
+        mot= jtextmot.getText();
+        sSQL="INSERT INTO devoluciones(iddevoluciones, producto, cantidad, fecha, cliente, motivo)"
+                +"VALUES(?,?,?,?,?,?)";
         
         try{
             PreparedStatement pst=cn.prepareStatement(sSQL);
@@ -289,6 +310,7 @@ public class devoluciones_producto extends javax.swing.JFrame {
             pst.setInt(3, can);
             pst.setString(4, date);
             pst.setString(5, cl);
+            pst.setString(6,mot);
             
             int n=pst.executeUpdate();
             if(n>0){
@@ -323,10 +345,11 @@ public class devoluciones_producto extends javax.swing.JFrame {
         
         //limpiar casillas
         
-                jtextcan.setText("");
+               jtextcan.setText("");
         jtextdate.setText("");
          cmbboxproducto.setSelectedIndex(0); //posicion cero es la primera
           cmbboxcliente.setSelectedIndex(0); //posicion cero es la primera
+          jtextmot.setText("");
          
     }//GEN-LAST:event_btnguardarActionPerformed
 
@@ -403,12 +426,14 @@ char numeros=evt.getKeyChar();
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel4;
+    private javax.swing.JLabel jLabel5;
     private javax.swing.JLabel jLabel6;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JToggleButton jToggleButton1;
     private javax.swing.JTextField jtextcan;
     private javax.swing.JTextField jtextdate;
+    private javax.swing.JTextField jtextmot;
     private javax.swing.JTable tbldevoluciones;
     // End of variables declaration//GEN-END:variables
     //Metodo que permite cargar los nombres de la tabla "producto" como elementos de un JComboBox
