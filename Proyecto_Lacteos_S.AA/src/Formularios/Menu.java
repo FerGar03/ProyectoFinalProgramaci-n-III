14833d62ad49fa2f5dfe9800e59b48ef9017baeb
Diego03 <dgarciar30@miumg.edu.gt>
2023-05-27 07:37:45 -0600

    ajustes pequeños
    algunos arreglos rápidos

--- Proyecto_Lacteos_S.AA/src/Formularios/Menu.java
+++ Proyecto_Lacteos_S.AA/src/Formularios/Menu.java
@@ -291,6 +291,6 @@ public class Menu extends javax.swing.JFrame {
     // End of variables declaration//GEN-END:variables
 
     public void setUsuario(String contraseña) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+       
     }
 }
